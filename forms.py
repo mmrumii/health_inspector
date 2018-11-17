abec23b4086a465acc2e1014292075ee044352bb
NasimRone <44744609+NasimRone@users.noreply.github.com>
2018-11-17 14:08:29 +0600

    Add files via upload
    updated form

--- forms.py
+++ forms.py
@@ -1,21 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SelectField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, DateField, SelectField, IntegerField, FloatField, SubmitField,TextAreaField
 from wtforms.validators import InputRequired, Email, Length
+from wtforms.fields.html5 import TelField, EmailField
+
+
+class UserRegistrationForm(FlaskForm):
+    FullName = StringField("",validators = [InputRequired(), Length(min=4, max=30)], render_kw={"placeholder": "Full Name"})
+    EmailAddress = EmailField("",validators =[InputRequired(), Email()], render_kw={"placeholder": "Email Address"})
+    UserType = SelectField("", validators=[InputRequired()], choices=[('Customer','Customer'),('S_provider','Service Provider')])
+    Username = StringField("",validators = [InputRequired(), Length(min=4, max=30)], render_kw={"placeholder": "@Username"})
+    Password = PasswordField("", validators=[InputRequired(), Length(min=8,max=50)], render_kw={"placeholder": "Password"})
+    Submit = SubmitField("Register")
+
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
+    Username = StringField("Username",validators = [InputRequired(), Length(min=4, max=30)], render_kw={"placeholder": "@Username"})
+    Password = PasswordField("Password", validators=[InputRequired(), Length(min=8,max=50)], render_kw={"placeholder": "Password"})
+    Remember = BooleanField('Remember me')
+    Submit = SubmitField("Login")
 
 
+class SearchForm(FlaskForm):
+    location = SelectField("", validators=[InputRequired()], choices=[('Bashundhara','Bashundhara'),('Uttara','Uttara')])
+    service = SelectField("", validators=[InputRequired()], choices=[('MRI','MRI'),('X-Ray','X-Ray')])
+    Submit = SubmitField("Search")
 
-class SignUpForm(FlaskForm):
-    firstname = StringField('firstname', validators=[InputRequired(), Length(min=3, max=50)])
-    lastname = StringField('lastname', validators = [InputRequired(), Length(min=3, max=50)])
-    phone = StringField('phone', validators=[InputRequired(), Length(min=8,max=50)])
-    user_type = SelectField("", validators=[InputRequired()], choices=[('Customer','Customer'),('S_provider','Service Provider')])
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    submit = SubmitField('submit')
 
+class ServiceForm(FlaskForm):
+    ServiceName = StringField("", validators=[InputRequired(), Length(min=2, max=100)], render_kw={"placeholder": "Name"})
+    Location = StringField("", validators=[InputRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Location"})
+    DetailsLocation = TextAreaField("", validators=[InputRequired(), Length(min=2, max=100)], render_kw={"placeholder": "details "})
+    HospitalName = StringField("", validators=[InputRequired(), Length(min=2, max=100)], render_kw={"placeholder": "hospital"})
+    Price = IntegerField("", render_kw={"placeholder": "price"})
+    Phone = StringField("", validators=[InputRequired(), Length(min=2, max=200)], render_kw={"placeholder": "phone"})
+    Submit = SubmitField("Create")
